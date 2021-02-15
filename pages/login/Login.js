72fccd9d8f82d6141117c83705bdfa7a19955be7
Federico Castro García <fede.castrog@gmail.com>
2021-02-15 00:47:57 -0300

    Style login

--- pages/login/Login.js
+++ pages/login/Login.js
@@ -1,24 +1,45 @@
+import cx from "classnames";
 import { getSession } from "next-auth/client";
 import { signIn } from "next-auth/client";
-import styles from "./Login.module.scss";
 import { FcGoogle } from "react-icons/fc";
+import Image from "next/image";
+import styles from "./Login.module.scss";
 
-const providers = [{ id: "google", name: "google" }];
+const providers = [{ id: "google", name: "Google" }];
 
 const Login = () => {
   return (
     <div className="layout">
       <div className={styles.container}>
-        <div className={styles.background}></div>
+        <div className={styles.background}>
+          <Image src="/assets/finances-6.svg" height={400} width={800} quality={100} />
+        </div>
         <div className={styles.login}>
-          {providers.map(provider => (
-            <div key={provider.name}>
-              <button className="button--main-action" onClick={() => signIn(provider.id)}>
-                Sign in with {provider.name}
-                <FcGoogle size={25} className="google" />
-              </button>
+          <div className={styles.header}>
+            <div className={styles["logo-container"]}>
+              <Image
+                src="/assets/logo.png"
+                width={40}
+                height={40}
+                alt="logo"
+                quality={100}
+              />
             </div>
-          ))}
+          </div>
+          <div className={cx(styles.providers)}>
+            <h2 className={styles.title}>Log in to your account</h2>
+            {providers.map(provider => (
+              <div key={provider.name}>
+                <button
+                  className={styles["button--main-action"]}
+                  onClick={() => signIn(provider.id)}
+                >
+                  Sign in with {provider.name}
+                  <FcGoogle size={25} className={styles.google} />
+                </button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -34,7 +55,7 @@ export async function getServerSideProps(ctx) {
     return {
       props: {},
       redirect: {
-        destination: "/feeds",
+        destination: "/main",
         permanent: false,
       },
     };
