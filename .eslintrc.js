474f1a9f5b51b657a0a12a8d5fecc1a62bad8d6d
Federico Castro García <fede.castrog@gmail.com>
2021-03-16 19:43:03 -0300

    Modify ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,4 +21,12 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
   },
+  overrides: [
+    {
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+      },
+    },
+  ],
 };
