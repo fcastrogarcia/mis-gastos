036efe763a6ccd21245435e45c94b14fb2eb9146
Federico Castro García <fede.castrog@gmail.com>
2021-03-16 16:44:28 -0300

    typescript initial config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
     es2021: true,
+    node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -12,7 +17,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react"],
+  plugins: ["react", "@typescript-eslint"],
   rules: {
     "react/react-in-jsx-scope": "off",
   },
