d847e84fa6fbe8d561c554f9666d90ff9563669b
Federico Castro García <fede.castrog@gmail.com>
2021-03-19 10:36:30 -0300

    Refactor period operations

--- context/calendar/operations.js
+++ context/calendar/operations.js
@@ -2,17 +2,14 @@ import types from "./types";
 import { getPeriod, getCurrentPeriod } from "utils/time";
 import addMonths from "date-fns/addMonths";
 
-export const incrementPeriod = (dispatch, state) => {
+export const selectPeriod = value => (dispatch, state) => {
   if (!state || !dispatch) throw new Error("No state or dispatch was provided");
-  const nextPeriod = addMonths(state.selectedPeriod, 1);
+  const nextPeriod = addMonths(state.selectedPeriod, value);
   return dispatch({ type: types.SET_SELECTED_PERIOD, payload: getPeriod(nextPeriod) });
 };
 
-export const decrementPeriod = (dispatch, state) => {
-  if (!state || !dispatch) throw new Error("No state or dispatch was provided");
-  const nextPeriod = addMonths(state.selectedPeriod, -1);
-  return dispatch({ type: types.SET_SELECTED_PERIOD, payload: getPeriod(nextPeriod) });
-};
+export const incrementPeriod = selectPeriod(1);
+export const decrementPeriod = selectPeriod(-1);
 
 export const setCurrentPeriod = dispatch => {
   if (!dispatch) throw new Error("Nodispatch was provided");
